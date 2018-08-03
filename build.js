74343e16c259646e13f504cfcb493da975c38520
tangshuang <mail@tangshuang.net>
2018-08-03 18:57:13 +0800

    Update build.js

--- build.js
+++ build.js
@@ -4,6 +4,30 @@ var webpack = require('webpack-stream')
 var webpackbufferify = require('webpack-bufferify')
 var gulpbufferify = require('gulp-bufferify')
 
+var config = {
+  mode: 'none',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'databaxe.bundle.js',
+    library: 'DataBaxe',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ]
+  },
+  plugins: [
+    webpackbufferify(function(content) {
+      content = content.replace('exports.default = DataBaxe;', 'module.exports = DataBaxe;')
+      return content
+    }),
+  ],
+}
+
 gulp.src(__dirname + '/src/**/*')
   .pipe(babel())
   .pipe(gulpbufferify(function(content, file) {
@@ -15,27 +39,18 @@ gulp.src(__dirname + '/src/**/*')
   .pipe(gulp.dest(__dirname + '/dist'))
 
 gulp.src(__dirname + '/src/databaxe.js')
-  .pipe(webpack({
-    mode: 'none',
-    output: {
-      path: __dirname + '/dist',
-      filename: 'databaxe.bundle.js',
-      library: 'DataBaxe',
-      libraryTarget: 'umd',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: 'babel-loader',
-        },
-      ]
-    },
-    plugins: [
-      webpackbufferify(function(content) {
-        content = content.replace('exports.default = DataBaxe;', 'module.exports = DataBaxe;')
-        return content
-      }),
-    ],
-  }))
+  .pipe(webpack(config))
+  .pipe(gulp.dest(__dirname + '/dist'))
+
+config.output.filename = 'databaxe.module.js'
+config.externals = [
+  'hello-async',
+  'hello-indexeddb',
+  'hello-worker',
+  'interpolate',
+  'lodash.merge',
+  'object-hashcode'
+]
+gulp.src(__dirname + '/src/databaxe.js')
+  .pipe(webpack(config))
   .pipe(gulp.dest(__dirname + '/dist'))
